f118058e055a0289c0c97d17f95cfe2090691baa
Mezaque <mezaquegit@gmail.com>
2024-10-31 20:58:17 -0300

    sm8550-common: Set voltageos maintainer
    Change-Id: I21b061f758e1e6fccd25642d10f9f49c449bf3e6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -168,7 +168,7 @@ BOOT_SECURITY_PATCH := 2024-10-01
 VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
 # SEPolicy
-include device/lineage/sepolicy/libperfmgr/sepolicy.mk
+include device/voltage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 PRODUCT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
